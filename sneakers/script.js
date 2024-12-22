93a241b5f75df177169692fea38a07daf2084663
hhormax <96630697+hhormax@users.noreply.github.com>
2024-12-22 20:13:54 +0300

    Add files via upload

---
+++ sneakers/script.js
@@ -0,0 +1,34 @@
+// (function ($) {
+//   var $dragMe = $(".dragme"),
+//     $container = $(".sl-container"),
+//     $viewAfter = $(".view-after");
+//   $dragMe.draggable({
+//     containment: "parent",
+//     drag: function () {
+//       $viewAfter.css({
+//         width: parseFloat($(this).css("left")) + 5,
+//       });
+//     },
+//   });
+//   $container.on("click", function (event) {
+//     var eventLeft = event.pageX - $container.offset().left - 15;
+//     animateTo(eventLeft);
+//   });
+//   animateTo("50%");
+//   function animateTo(_left) {
+//     $dragMe.animate(
+//       {
+//         left: _left,
+//       },
+//       "slow",
+//       "linear"
+//     );
+//     $viewAfter.animate(
+//       {
+//         width: _left,
+//       },
+//       "slow",
+//       "linear"
+//     );
+//   }
+// })(jQuery);
